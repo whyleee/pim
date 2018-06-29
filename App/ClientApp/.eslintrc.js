94341fd9941a90f8d0d245f298d62a943631fd89
Pavel Nezhencev <pavel.nezhencev@creuna.com>
2018-06-29 23:30:30 +0300

    Relax eslint rules

--- App/ClientApp/.eslintrc.js
+++ App/ClientApp/.eslintrc.js
@@ -9,7 +9,12 @@ module.exports = {
   ],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+
+    // airbnb base overrides
+    'comma-dangle': ['error', 'never'], // trailing commas is too much
+    'eqeqeq': 'off', // too strict, == is ok almost in all cases
+    'semi': ['error', 'never'], // you don't need semis
   },
   parserOptions: {
     parser: 'babel-eslint'
