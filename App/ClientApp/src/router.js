cfe9d5d60b6d7de4f9b6c19627d9aa8841f8b473
Pavel Nezhencev <pavel.nezhencev@creuna.com>
2018-08-14 01:22:15 +0300

    Merge home view into product list

--- App/ClientApp/src/router.js
+++ App/ClientApp/src/router.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from '@/views/Home.vue'
+import ProductList from '@/views/ProductList.vue'
 import ProductEdit from '@/views/ProductEdit.vue'
 
 Vue.use(Router)
@@ -11,7 +11,7 @@ export default new Router({
     {
       path: '/',
       name: 'home',
-      component: Home
+      component: ProductList
     },
     {
       path: '/:id',
--- App/ClientApp/src/views/ProductList.vue
+++ App/ClientApp/src/views/ProductList.vue
@@ -1,31 +1,44 @@
 <template>
-  <b-list-group flush>
-    <b-list-group-item
-      v-for="product in products"
-      :key="product.id"
-    >
-      <b-link :to="{ name: 'product', params: { id: product.id }}">
-        {{ product.name }}
-      </b-link>
-      <b-link
-        class="float-right"
-        @click.stop="confirmDelete(product)"
-      >
-        Delete
-      </b-link>
-    </b-list-group-item>
+  <div>
+    <h1>Products</h1>
+    <b-card no-body>
+      <div slot="header">
+        <b-button-toolbar>
+          <b-button-group>
+            <b-btn :to="{ name: 'product', params: { id: 'new' }}">New</b-btn>
+          </b-button-group>
+        </b-button-toolbar>
+      </div>
 
-    <b-modal
-      :visible="!!selectedProduct"
-      title="Delete item"
-      @ok="onDeleteModalOk"
-      @cancel="onDeleteModalCancel"
-    >
-      <p v-if="selectedProduct">
-        Are you sure you want to delete product "{{ selectedProduct.name }}"?
-      </p>
-    </b-modal>
-  </b-list-group>
+      <b-list-group flush>
+        <b-list-group-item
+          v-for="product in products"
+          :key="product.id"
+        >
+          <b-link :to="{ name: 'product', params: { id: product.id }}">
+            {{ product.name }}
+          </b-link>
+          <b-link
+            class="float-right"
+            @click.stop="confirmDelete(product)"
+          >
+            Delete
+          </b-link>
+        </b-list-group-item>
+
+        <b-modal
+          :visible="!!selectedProduct"
+          title="Delete item"
+          @ok="onDeleteModalOk"
+          @cancel="onDeleteModalCancel"
+        >
+          <p v-if="selectedProduct">
+            Are you sure you want to delete product "{{ selectedProduct.name }}"?
+          </p>
+        </b-modal>
+      </b-list-group>
+    </b-card>
+  </div>
 </template>
 
 <script>
