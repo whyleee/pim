9011b880e6659ec6e6189b72e4daa96243c703f4
Pavel Nezhencev <pavel.nezhencev@creuna.com>
2018-08-14 01:19:12 +0300

    Move product list and edit to views

--- App/ClientApp/src/router.js
+++ App/ClientApp/src/router.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/views/Home.vue'
-import ProductEdit from '@/components/ProductEdit.vue'
+import ProductEdit from '@/views/ProductEdit.vue'
 
 Vue.use(Router)
 
